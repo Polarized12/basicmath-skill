dec3d15185f73e1cbcb54ef680ce4d7dad9d6dad
Polarized12 <55970172+Polarized12@users.noreply.github.com>
2020-05-14 21:14:49 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,30 @@
 from mycroft import MycroftSkill, intent_file_handler
 
-
-class Basicmath(MycroftSkill):
+class SMathSkill(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super().__init__()
 
-    @intent_file_handler('basicmath.intent')
-    def handle_basicmath(self, message):
-        self.speak_dialog('basicmath')
+    @intent_handler('smath.intent')
+    def handle_operation(self, message):
+        operator = message.data.get('operator')
+	n1 = message.data.get('number')
+	n2 = message.data.get('number')
+	if operator is not None:
+		if operator == "plus" or operator == "add" or operator == "added to":
+			n3 = n1 + n2
+		if operator == "minus" or operator == "subtract":
+			n3 = n1 - n2
+		elif operator == "subtracted from":
+			n3 = n2 - n1
+		if operator == "multiply" or operator == "times" or operator == "multiplied by":
+			n3 = n1 * n2
+		if operator == "divide" or operator == "over" or operator == "divided by":
+			n3 = n1 / n2
+		self.speak_(n3)
 
+    def stop(self):
+        pass
 
 def create_skill():
-    return Basicmath()
+    return SMathSkill()
 
